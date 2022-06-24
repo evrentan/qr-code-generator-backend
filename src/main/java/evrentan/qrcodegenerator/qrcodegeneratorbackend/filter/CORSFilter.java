e2ac505802317aa8abf3688f15675b24da55c90f
Evren Tan <evrentan@gmail.com>
2022-06-24 17:02:24 +0300

    Refactor CORSFilter class
    Refactor CORSFilter class

--- src/main/java/evrentan/qrcodegenerator/qrcodegeneratorbackend/filter/CORSFilter.java
+++ src/main/java/evrentan/qrcodegenerator/qrcodegeneratorbackend/filter/CORSFilter.java
@@ -1,12 +1,15 @@
 package evrentan.qrcodegenerator.qrcodegeneratorbackend.filter;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * CORSFilter class implementing Filter library of Javax Servlet
@@ -19,10 +22,9 @@ public class CORSFilter implements Filter {
   /**
    * Generate Byte Array Output Stream according to the given request
    *
-   * @param servletRequest HTTP Servlet Request
+   * @param servletRequest  HTTP Servlet Request
    * @param servletResponse HTTP Servlet Response
-   * @param filterChain filter chain object from servlet container to filter the request
-   *
+   * @param filterChain     filter chain object from servlet container to filter the request
    * @author <a href="https://github.com/evrentan">Evren Tan</a>
    * @since 1.0.0
    */
@@ -32,18 +34,18 @@ public class CORSFilter implements Filter {
     HttpServletRequest request = (HttpServletRequest) servletRequest;
     HttpServletResponse response = (HttpServletResponse) servletResponse;
 
-    response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, PATCH, DELETE, OPTIONS");
-    response.setHeader("Access-Control-Max-Age", "3600");
+    response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, String.join(",", HttpMethod.POST.name(), HttpMethod.GET.name(), HttpMethod.PUT.name(), HttpMethod.PATCH.name(), HttpMethod.DELETE.name(), HttpMethod.OPTIONS.name()));
+    response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, String.join(",", HttpHeaders.AUTHORIZATION, HttpHeaders.ORIGIN, HttpHeaders.ACCEPT, HttpHeaders.CONTENT_TYPE, HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD, HttpHeaders.ACCESS_CONTROL_REQUEST_HEADERS));
+    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 
-    response.setHeader("Access-Control-Allow-Headers", "Authorization, Origin, Accept, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Collation");
-    response.setContentType("application/json");
-
+    final String origin = request.getHeader(HttpHeaders.ORIGIN);
+    if (Objects.nonNull(origin))
+      response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, origin);
 
     if (request.getMethod().equalsIgnoreCase(HttpMethod.OPTIONS.name())) {
-        response.getWriter().print(HttpStatus.OK.name());
-        response.getWriter().flush();
+      response.getWriter().print(HttpStatus.OK.name());
+      response.getWriter().flush();
     } else {
-      response.setHeader("Access-Control-Allow-Origin", "*");
       filterChain.doFilter(request, response);
     }
   }

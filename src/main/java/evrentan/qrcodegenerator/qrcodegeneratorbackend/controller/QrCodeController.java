8958b36e25043bbd792ffe6fcaee20d8fbfc00b7
Evren Tan <evren.tan@etiya.com>
2022-10-25 18:10:27 +0300

    Fix AllArgsConstructor Not Found Issue
    Fix AllArgsConstructor Not Found Issue
    
    - Add lombok.AllArgsConstructor import

--- src/main/java/evrentan/qrcodegenerator/qrcodegeneratorbackend/controller/QrCodeController.java
+++ src/main/java/evrentan/qrcodegenerator/qrcodegeneratorbackend/controller/QrCodeController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
